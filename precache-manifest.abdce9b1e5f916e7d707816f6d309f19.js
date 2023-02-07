06330b61c1417018d5b830cf7fbe1d703ff71f41
Alena <olenanepokryta1997@gmail.com>
2023-02-07 18:14:24 +0200

    Updates

---
+++ precache-manifest.abdce9b1e5f916e7d707816f6d309f19.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "64ff066977e1d9412a3e",
+    "url": "/healthy-food_internship-react_ONIX/static/css/main.ae318e13.chunk.css"
+  },
+  {
+    "revision": "64ff066977e1d9412a3e",
+    "url": "/healthy-food_internship-react_ONIX/static/js/main.64ff0669.chunk.js"
+  },
+  {
+    "revision": "8054edc2e706a6ade2b1",
+    "url": "/healthy-food_internship-react_ONIX/static/js/1.8054edc2.chunk.js"
+  },
+  {
+    "revision": "be74231d6deb12efda9d",
+    "url": "/healthy-food_internship-react_ONIX/static/js/runtime~main.be74231d.js"
+  },
+  {
+    "revision": "546a71e9bb778ecfe7d33ffb660d4866",
+    "url": "/healthy-food_internship-react_ONIX/static/media/arrow.546a71e9.svg"
+  },
+  {
+    "revision": "d4204e65ba536fd8b487fa5731989f11",
+    "url": "/healthy-food_internship-react_ONIX/static/media/fish.d4204e65.png"
+  },
+  {
+    "revision": "eb285362f2cd0bb9d2770699e28cf048",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ic_call.eb285362.svg"
+  },
+  {
+    "revision": "b043f07004ad7379f2cb9f262f3093fd",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ic_near_me.b043f070.svg"
+  },
+  {
+    "revision": "dd768d5c3e127fedebcc25b05482501b",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ic_watch_later.dd768d5c.svg"
+  },
+  {
+    "revision": "ad7d22111aa81c0db05479562e13dc0e",
+    "url": "/healthy-food_internship-react_ONIX/static/media/logo-title.ad7d2211.svg"
+  },
+  {
+    "revision": "39a2cf498dc68fe172f965f5640762db",
+    "url": "/healthy-food_internship-react_ONIX/static/media/logo.39a2cf49.svg"
+  },
+  {
+    "revision": "16f03def812764e968fb06924c4e6918",
+    "url": "/healthy-food_internship-react_ONIX/static/media/beans-and-peppers.16f03def.png"
+  },
+  {
+    "revision": "e89dbfd18b8c7e337840d5b798090a4e",
+    "url": "/healthy-food_internship-react_ONIX/static/media/big-img1.e89dbfd1.png"
+  },
+  {
+    "revision": "99fe9261904e92d2a89f3a60bd9d8624",
+    "url": "/healthy-food_internship-react_ONIX/static/media/big-img2.99fe9261.png"
+  },
+  {
+    "revision": "ab823321b57e06eff56cabec97883bbf",
+    "url": "/healthy-food_internship-react_ONIX/static/media/big-img3.ab823321.png"
+  },
+  {
+    "revision": "ffed3d5d038e4fb96fb9fb4948f60b79",
+    "url": "/healthy-food_internship-react_ONIX/static/media/eggs.ffed3d5d.png"
+  },
+  {
+    "revision": "2e3e6975b76466fadcef77f5c5520655",
+    "url": "/healthy-food_internship-react_ONIX/static/media/green-salad.2e3e6975.png"
+  },
+  {
+    "revision": "99d1f997200c11fe27c48a3b6dea1a36",
+    "url": "/healthy-food_internship-react_ONIX/static/media/img1-2.99d1f997.png"
+  },
+  {
+    "revision": "021e766a8ff3255bc2dc81cc0886a258",
+    "url": "/healthy-food_internship-react_ONIX/static/media/img1-3.021e766a.png"
+  },
+  {
+    "revision": "1209f3a984a6bd4674a2290018fceb17",
+    "url": "/healthy-food_internship-react_ONIX/static/media/img2-3.1209f3a9.png"
+  },
+  {
+    "revision": "65ce2d5ce2b74b21ba59966a49c4dfa0",
+    "url": "/healthy-food_internship-react_ONIX/static/media/img3-2.65ce2d5c.png"
+  },
+  {
+    "revision": "83e540bc7c49c8eebab03fcee972b9c1",
+    "url": "/healthy-food_internship-react_ONIX/static/media/img3-3.83e540bc.png"
+  },
+  {
+    "revision": "dba221377327807cef4e28209a718ff2",
+    "url": "/healthy-food_internship-react_ONIX/static/media/placehholder1.dba22137.png"
+  },
+  {
+    "revision": "1c79fa2cdcb183659a0a4f1db3487aa8",
+    "url": "/healthy-food_internship-react_ONIX/static/media/placehholder2.1c79fa2c.png"
+  },
+  {
+    "revision": "b607899d99f8fcac228ee3c0b5a116fc",
+    "url": "/healthy-food_internship-react_ONIX/static/media/placehholder3.b607899d.png"
+  },
+  {
+    "revision": "11cc4f691759afb6e349b8e20e8fc086",
+    "url": "/healthy-food_internship-react_ONIX/static/media/placehholder4.11cc4f69.png"
+  },
+  {
+    "revision": "fa364ac619564c84b3dff1d2f3b4cbfa",
+    "url": "/healthy-food_internship-react_ONIX/static/media/placehholder5.fa364ac6.png"
+  },
+  {
+    "revision": "1b1a8cbf0fb94250342c91aec36c87d6",
+    "url": "/healthy-food_internship-react_ONIX/static/media/placehholder6.1b1a8cbf.png"
+  },
+  {
+    "revision": "6347edca6995fab64810f58cc17a94d8",
+    "url": "/healthy-food_internship-react_ONIX/static/media/salad-vegetables.6347edca.png"
+  },
+  {
+    "revision": "715aa5449215834999ea1d98f99e16bb",
+    "url": "/healthy-food_internship-react_ONIX/static/media/salad.715aa544.png"
+  },
+  {
+    "revision": "063f2af11bb0ebe56d7e734186103393",
+    "url": "/healthy-food_internship-react_ONIX/static/media/three-salads.063f2af1.png"
+  },
+  {
+    "revision": "a24a2792ddd5c771056e5dc3ba5cc6e1",
+    "url": "/healthy-food_internship-react_ONIX/static/media/vegetables.a24a2792.png"
+  },
+  {
+    "revision": "6b29b9c7d810fe2babcad351d20ad6e6",
+    "url": "/healthy-food_internship-react_ONIX/static/media/yellow-soup.6b29b9c7.png"
+  },
+  {
+    "revision": "8e82c0d01a44ea894573409364f5a290",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ABOUT.8e82c0d0.svg"
+  },
+  {
+    "revision": "0a9bae1d3f98dadad4cd838ca07f949e",
+    "url": "/healthy-food_internship-react_ONIX/static/media/CHEFS.0a9bae1d.svg"
+  },
+  {
+    "revision": "275e4929399abdcc1d44e7e85abfa782",
+    "url": "/healthy-food_internship-react_ONIX/static/media/DISHES.275e4929.svg"
+  },
+  {
+    "revision": "ff01a9ed915f0f882e52090746fdd9f6",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ic_facebook.ff01a9ed.svg"
+  },
+  {
+    "revision": "2324bea5d6c8a90ad31b966c012f5df1",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ic_instagram.2324bea5.svg"
+  },
+  {
+    "revision": "44264db62777989fdc48529dd80d1475",
+    "url": "/healthy-food_internship-react_ONIX/static/media/ic_twitter.44264db6.svg"
+  },
+  {
+    "revision": "eb48eaec4d9602b5178b58b0d3d288c5",
+    "url": "/healthy-food_internship-react_ONIX/static/media/RECIPES.eb48eaec.svg"
+  },
+  {
+    "revision": "0497e0792c9fa2cf5e5744a4ffe780fc",
+    "url": "/healthy-food_internship-react_ONIX/static/media/SOCIAL.0497e079.svg"
+  },
+  {
+    "revision": "a3722fa47f091db0565a937fa525573d",
+    "url": "/healthy-food_internship-react_ONIX/static/media/WORK.a3722fa4.svg"
+  },
+  {
+    "revision": "e914a2dd1cd1d3c3944e0ad0e950046d",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Light.e914a2dd.woff"
+  },
+  {
+    "revision": "990d529761b45e6cff159655a2eac3c9",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Light.990d5297.eot"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Light.30778183.ttf"
+  },
+  {
+    "revision": "2a615283a212836fd6c33bf32bc87ebf",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Medium.2a615283.woff"
+  },
+  {
+    "revision": "ae503b13b400cfaea50d767b4b61b810",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Medium.ae503b13.eot"
+  },
+  {
+    "revision": "ca12a719cd344dd391d9f51e7bbd9b33",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Bold.ca12a719.woff"
+  },
+  {
+    "revision": "e603c690f754c70654004a753c17b60b",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Medium.e603c690.ttf"
+  },
+  {
+    "revision": "50475e9398305457c4ead55941b10a0f",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Bold.50475e93.ttf"
+  },
+  {
+    "revision": "5bbf832fec501cfb6afae24d67d39a0a",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Bold.5bbf832f.eot"
+  },
+  {
+    "revision": "303f6a538da65cd9cd71f5cdd0d92125",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Black.303f6a53.ttf"
+  },
+  {
+    "revision": "d80c62276301650b8e397370378ac88f",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Black.d80c6227.woff"
+  },
+  {
+    "revision": "00b41bafdbc3722a4515887e8e4dde6e",
+    "url": "/healthy-food_internship-react_ONIX/static/media/HelveticaNeueCyr-Black.00b41baf.eot"
+  },
+  {
+    "revision": "f88572b27c8d1e9a88543b0d0742d5d4",
+    "url": "/healthy-food_internship-react_ONIX/static/media/main-photo.f88572b2.png"
+  },
+  {
+    "revision": "fa9c612463e3a65bacc8d932353c1b16",
+    "url": "/healthy-food_internship-react_ONIX/index.html"
+  }
+];
