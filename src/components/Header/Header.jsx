2a4b14db7faffbb2fb2663a6e4c1a8ab1f2796b4
Alena <olenanepokryta1997@gmail.com>
2023-03-03 14:24:42 +0200

    feat: [LSS-4] create a new Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import Logo from '../Logo/Logo';
+
+import './sass/Header.sass';
+
+function Header() {
+  return (
+    <header className="header">
+      <Logo className="logo" />
+      <nav className="nav">
+        <a className="nav_link" href="#menu">Menu</a>
+        <a className="nav_link" href="#recipes">Recipes</a>
+        <a className="nav_link" href="#chefs">Chefs</a>
+        <a className="nav_link" href="#contact">Contacts</a>
+      </nav>
+    </header>
+        
+  );
+}
+
+export default Header;
