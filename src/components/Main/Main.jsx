02eb16b13ee202a1e13056ff33810e9f85399076
Alena <olenanepokryta1997@gmail.com>
2023-03-03 15:48:53 +0200

    feat: [LSS-4] create a new Main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,28 @@
+import Contacts from '../Contacts/Contacts';
+
+import './sass/Main.sass';
+
+function Main() {
+  return (
+    <>
+      <h1 className="title">
+        <span>Your</span>
+        {' '}
+        favourite food
+        <br />
+        <span>delivered</span>
+        {' '}
+        hot & fresh
+      </h1>
+      <h3 className="subtitle">
+        HEALTHY SWITCHER chefs do all the prep work, like peeling, chopping & 
+        marinating, so you can cook a fresh homemade dinner in just 15 minutes.
+      </h3>
+      <button className="main__btn button" type="submit">Order Now</button>
+      <Contacts />
+    </>
+        
+  );
+}
+
+export default Main;
