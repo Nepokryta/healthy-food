476f9e54b91d6715c8de6f3dcdc3d14e90f1f296
Alena <olenanepokryta1997@gmail.com>
2023-03-08 01:57:47 +0200

    fix: [LSS-4] fix stateful and stateless component   DishCardsView

--- src/components/DishCards/DishCardView.jsx
+++ src/components/DishCards/DishCardView.jsx
@@ -6,46 +6,109 @@ import { ReactComponent as Close } from '../../assets/icons/close.svg';
 
 import './sass/DishCardView.sass';
 
-class DishCardView extends Component {                  
+class DishCardView extends Component {
   render() {
     const {
-      id, src, alt, title, subtitle, newSubtitle, description, 
-      onCardClick, onAddElement, onDeleteElement, onAddElementOnClick, onAddCardClick
+      dish, onSort, onRandomSort, onCardClick, onAddElement, 
+      onDeleteElement, onAddElementOnClick, onAddCardClick 
     } = this.props;
-
+    const elements = dish.map((item) => (
+      <DishCard
+        key={item.key} 
+        id={item.key} 
+        src={item.src}
+        alt={item.alt}
+        title={item.title}
+        subtitle={item.subtitle}
+        newSubtitle={item.newSubtitle}
+        description={item.description}
+        onSort={onSort}
+        onRandomSort={onRandomSort}
+        onCardClick={onCardClick} 
+        onAddElement={onAddElement}
+        onDeleteElement={onDeleteElement}
+        onAddElementOnClick={onAddElementOnClick}
+        onAddCardClick={onAddCardClick}
+      />
+    ));
+      
     return (
-      <div className="dish__card">
-        <button className="arrow-card" type="submit" onClick={() => onCardClick(id)}>
-          <Close />
-        </button>
-        <img src={src} alt={alt} />
-        <div className="dish__card-details">
-          <h2 className="dish__card-title">{title}</h2>
-          <h3 className="dish__card-subtitle">{subtitle}</h3>
-          <h3 className="dish__card-newsubtitle">{newSubtitle}</h3>
-          <h4 className="dish__card-description">{description}</h4>
-          <button className="card__action-btn button" type="submit" onClick={() => onAddElement(id)}>MY FAVORITE</button>
-          <div className="botton_block">
-            <button className="card__action-btn button" type="submit" onClick={() => onDeleteElement(id)}>DELETE</button>
-            <button className="card__action-btn button" type="submit" onClick={() => onAddElementOnClick(id)}>ADD</button>
-          </div>
+      <div className="container">
+        <div className="dish__cards">
+          {elements}
         </div>
-        <div className="dish__card-action">
-          <RatingStars />
-          <button 
-            className="card__action-btn button" 
-            type="submit"
-            onClick={() => onAddCardClick(src, alt, title, subtitle, description)}
-          >
-            ADD CARD
-          </button>
+        <div className="sortBtn">
+          <button className="action-btn button" type="submit" onClick={this.onRandomSort}>RANDOM</button>
+          <button className="action-btn button" type="submit" onClick={this.onSort}>SORT</button>
         </div>
       </div>
     );
   }
 }
 
+function DishCard(props) {                  
+  const {
+    id, src, alt, title, subtitle, newSubtitle, description, 
+    onCardClick, onAddElement, onDeleteElement, onAddElementOnClick, onAddCardClick
+  } = props;
+  return (
+    <div className="dish__card">
+      <button className="arrow-card" type="submit" onClick={() => onCardClick(id)}>
+        <Close />
+      </button>
+      <img src={src} alt={alt} />
+      <div className="dish__card-details">
+        <h2 className="dish__card-title">{title}</h2>
+        <h3 className="dish__card-subtitle">{subtitle}</h3>
+        <h3 className="dish__card-newsubtitle">{newSubtitle}</h3>
+        <h4 className="dish__card-description">{description}</h4>
+        <button className="card__action-btn button" type="submit" onClick={() => onAddElement(id)}>MY FAVORITE</button>
+        <div className="botton_block">
+          <button className="card__action-btn button" type="submit" onClick={() => onDeleteElement(id)}>DELETE</button>
+          <button className="card__action-btn button" type="submit" onClick={() => onAddElementOnClick(id)}>ADD</button>
+        </div>
+      </div>
+      <div className="dish__card-action">
+        <RatingStars />
+        <button 
+          className="card__action-btn button" 
+          type="submit"
+          onClick={() => onAddCardClick(src, alt, title, subtitle, description)}
+        >
+          ADD CARD
+        </button>
+      </div>
+    </div>
+  );
+}
+
 DishCardView.propTypes = {
+  onSort: PropTypes.func.isRequired, 
+  onRandomSort: PropTypes.func.isRequired, 
+  onCardClick: PropTypes.func.isRequired, 
+  onAddElement: PropTypes.func.isRequired,
+  onDeleteElement: PropTypes.func.isRequired,
+  onAddElementOnClick: PropTypes.func.isRequired,
+  onAddCardClick: PropTypes.func.isRequired,
+  dish: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired, 
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      newSubtitle: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      onCardClick: PropTypes.func.isRequired, 
+      onAddElement: PropTypes.func.isRequired,
+      onDeleteElement: PropTypes.func.isRequired,
+      onAddElementOnClick: PropTypes.func.isRequired,
+      onAddCardClick: PropTypes.func.isRequired,
+    }).isRequired
+  ).isRequired,
+};
+
+DishCard.propTypes = {
   id: PropTypes.number.isRequired, 
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
