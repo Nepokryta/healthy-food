3f465387fa49c35c9ee64beb12eaf77c706e8f2d
Alena <olenanepokryta1997@gmail.com>
2023-03-22 02:32:05 +0200

    feat: [LSS-7] create HOC component withLoadingAndError

---
+++ src/hoc/withLoadingAndError.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+import EdamamService from '../services/EdamamService';
+import Spinner from '../components/Spinner/Spinner';
+import ErrorRobot from '../assets/img/error_robot.png';
+
+export default function withLoadingAndError(MyComponent) {
+  return class extends Component {
+    edamamService = new EdamamService();
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoаding: true,
+        data: null,
+        error: null,
+      };
+    }
+
+    componentDidMount() {
+      try {
+        this.edamamService.getCards()
+          .then((data) => {
+            this.setState({
+              data,
+              isLoаding: false,
+            });
+          });
+      } catch (error) {
+        this.setState({
+          error,
+          isLoаding: false,
+        });
+      } 
+    }
+
+    render() {
+      const { data, isLoаding, error } = this.state;
+      if (isLoаding) {
+        return <Spinner />;
+      }
+      
+      if (error) {
+        console.log('Error', error);
+        return <ErrorRobot />;
+      }
+      return <MyComponent data={data} />;
+    }
+  };
+}
