99fe2f5365cdfe5cf0ac3985517bf9e0db2ec974
Alena <olenanepokryta1997@gmail.com>
2023-03-22 02:40:14 +0200

    feat: [LSS-7] connect HOC to RecipeCards component and fix from functional component to class component

--- src/components/RecipeCards/RecipeCards.jsx
+++ src/components/RecipeCards/RecipeCards.jsx
@@ -1,37 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import RecipeCardView from './RecipeCardView';
+import withLoadingAndError from '../../hoc/withLoadingAndError';
 
 import './sass/RecipeCards.sass';
 
-function RecipeCards({ recipes }) {
-  const elements = recipes.map((item) => (
-    <RecipeCardView 
-      key={item.key} 
-      size={item.size} 
-      background={item.background} 
-      subtitle={item.subtitle} 
-      title={item.title}
-    />
-  ));
+class RecipeCards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [],
+    };
+  }
+
+  componentDidMount() {
+    const { data } = this.props;
+    const updatedRecipes = data
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 4)
+      .map((item) => ({ 
+        ...item, 
+        key: item.id, 
+      }));
+    this.setState({ 
+      recipes: updatedRecipes,
+    });
+  }
+
+  render() {
+    const { recipes } = this.state;
     
-  return (
-    <div className="recipes__cards">
-      {elements}
-    </div> 
-  );
+    const elements = recipes.map((item, index) => (
+      <RecipeCardView 
+        key={item.key} 
+        src={item.src}
+        alt={item.alt}
+        size={index === 0 ? 'big' : 'small'}
+        subtitle={item.subtitle} 
+        title={item.title}
+        newSubtitle={item.newSubtitle}
+        linkToRecipe={item.linkToRecipe}
+        totalTime={item.totalTime}
+        totalWeight={item.totalWeight}
+      />
+    ));
+    
+    return (
+      <div className="recipes__cards">
+        {elements}
+      </div> 
+    );
+  }
 }
 
 RecipeCards.propTypes = {
-  recipes: PropTypes.arrayOf(
+  data: PropTypes.arrayOf(
     PropTypes.shape({
-      key: PropTypes.number.isRequired,
-      size: PropTypes.string.isRequired,
-      background: PropTypes.string.isRequired,
-      subtitle: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired, 
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
-    })
+      subtitle: PropTypes.string.isRequired,
+      newSubtitle: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      showElement: PropTypes.bool
+    }).isRequired
   ).isRequired,
 };
 
-export default RecipeCards;
+export default withLoadingAndError(RecipeCards);
