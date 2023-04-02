a9e06abc0a9af76d8202ecdff946d13ab2b78aff
Alena <olenanepokryta1997@gmail.com>
2023-04-02 14:16:10 +0300

    feat: [LSS-8] add Context.Consumer on Headline

--- src/components/Headline/Headline.jsx
+++ src/components/Headline/Headline.jsx
@@ -1,21 +1,30 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ThemeContext from '../ThemeContext/ThemeContext';
 
 import './sass/Headline.sass';
 
-function Headline({
-  title, src, alt, greenLine 
-}) {
-  return (
-    <>
-      <div className="headline">
-        <h2 className="headline__title">{title}</h2>
-        <div className="headline__subtitle">
-          <img src={src} alt={alt} />
-        </div>
-      </div>
-      <span className={`green__line${greenLine}`} />
-    </>
-  );
+class Headline extends Component {
+  render() {
+    const {
+      title, src, alt, greenLine 
+    } = this.props;
+    return (
+      <ThemeContext.Consumer>
+        {(theme) => (
+          <>
+            <div className="headline">
+              <h2 className={`headline__title ${theme}`}>{title}</h2>
+              <div className="headline__subtitle">
+                <img src={src} alt={alt} />
+              </div>
+            </div>
+            <span className={`green__line${greenLine}`} />
+          </>
+        )}
+      </ThemeContext.Consumer>   
+    );
+  }
 }
 
 Headline.propTypes = {
