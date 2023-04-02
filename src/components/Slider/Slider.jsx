a5cd6a6ff397df05bd38c67e3d1b51ad4958ef76
Alena <olenanepokryta1997@gmail.com>
2023-04-02 14:16:51 +0300

    feat: [LSS-8] add Context.Consumer on Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ThemeContext from '../ThemeContext/ThemeContext';
 
 import SliderView from './SliderView';
 import Arrow from '../../assets/icons/arrow.svg';
 
 import './sass/Slider.sass';
 
-function Slider({ slider }) {
-  const elements = slider.map((item) => (
-    <SliderView key={item.key} src={item.src} alt={item.alt} />
-  ));
-
-  return (
-    <div className="about__slider">
-      <h3 className="about__subtitle">
-        In aliqua ea ullamco ad est ex non deserunt nulla. 
-        Consectetur sint ea aliquip aliquip consectetur 
-        voluptate est. Eu minim dolore laboris enim mollit 
-        voluptate irure esse aliquip.
-      </h3>
-      <div className="cards">
-        {elements}
-      </div>
-      <button className="arrow_btn arrow_btn-left" type="submit">
-        <img src={Arrow} alt="arrow" />
-      </button>
-      <button className="arrow_btn arrow_btn-right" type="submit">
-        <img src={Arrow} alt="arrow" />
-      </button>
-    </div>
-  );
+class Slider extends Component {
+  render() {
+    const { slider } = this.props;
+    const elements = slider.map((item) => (
+      <SliderView key={item.key} src={item.src} alt={item.alt} />
+    ));
+      
+    return (
+      <ThemeContext.Consumer>
+        {(theme) => (
+          <div className="about__slider">
+            <h3 className={`about__subtitle ${theme}`}>
+              In aliqua ea ullamco ad est ex non deserunt nulla. 
+              Consectetur sint ea aliquip aliquip consectetur 
+              voluptate est. Eu minim dolore laboris enim mollit 
+              voluptate irure esse aliquip.
+            </h3>
+            <div className="cards">
+              {elements}
+            </div>
+            <button className={`arrow_btn ${theme} arrow_btn-left`} type="submit">
+              <img src={Arrow} alt="arrow" />
+            </button>
+            <button className={`arrow_btn ${theme} arrow_btn-right`} type="submit">
+              <img src={Arrow} alt="arrow" />
+            </button>
+          </div>
+        )}
+      </ThemeContext.Consumer> 
+    );
+  }
 }
+Slider.contextType = ThemeContext;
 
 Slider.propTypes = {
   slider: PropTypes.arrayOf(
