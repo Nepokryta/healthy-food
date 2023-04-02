1b2476a861fcb233591c7b87e0b01e9f4e537885
Alena <olenanepokryta1997@gmail.com>
2023-04-02 14:38:47 +0300

    feat: [LSS-8] add Context.Consumer on WorksCardView

--- src/components/WorksCards/WorksCardView.jsx
+++ src/components/WorksCards/WorksCardView.jsx
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
+import ThemeContext from '../ThemeContext/ThemeContext';
 
 import './sass/WorksCardView.sass';
 
 function WorksCardView({ title, subtitle }) {
   return (
-    <div className="work__card">
-      <h2 className="work__card-title">{title}</h2>
-      <span className="work__card-line" />
-      <h3 className="work__card-subtitle">{subtitle}</h3>
-    </div>
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <div className={`work__card ${theme}`}>
+          <h2 className={`work__card-title ${theme}`}>{title}</h2>
+          <span className="work__card-line" />
+          <h3 className={`work__card-subtitle ${theme}`}>{subtitle}</h3>
+        </div>
+      )}
+    </ThemeContext.Consumer>     
   );
 }
   
