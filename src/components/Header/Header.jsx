9f87b547344b8271eb8dbed248384f874a85bdf4
Alena <olenanepokryta1997@gmail.com>
2023-04-03 02:34:56 +0300

    fix: [LSS-8] fix header button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from '../ThemeContext/ThemeContext';
 import Logo from '../Logo/Logo';
+import toggleOff from '../../assets/icons/toggle-off.svg';
+import toggleOn from '../../assets/icons/toggle-on.svg';
 
 import './sass/Header.sass';
 
@@ -13,7 +15,9 @@ class Header extends Component {
         {(theme) => (
           <header className="header">
             <Logo className="logo" />
-            <button type="button" onClick={toggleTheme}>Toggle Theme</button>
+            <button type="button" onClick={toggleTheme} className={`header_button ${theme}`}>
+              {theme === 'dark' ? <img src={toggleOff} alt="toggleOff" /> : <img src={toggleOn} alt="toggleOn" /> } 
+            </button>
             <nav className="nav">
               <a className={`nav_link ${theme}`} href="#menu">Menu</a>
               <a className={`nav_link ${theme}`} href="#recipes">Recipes</a>
