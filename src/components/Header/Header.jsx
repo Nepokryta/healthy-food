7ae80b024a6965ad552a34ab022ff8f379e1be29
Alena <olenanepokryta1997@gmail.com>
2023-04-02 14:15:37 +0300

    feat: [LSS-8] add Context.Consumer on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ThemeContext from '../ThemeContext/ThemeContext';
 import Logo from '../Logo/Logo';
 
 import './sass/Header.sass';
 
-function Header() {
-  return (
-    <header className="header">
-      <Logo className="logo" />
-      <nav className="nav">
-        <a className="nav_link" href="#menu">Menu</a>
-        <a className="nav_link" href="#recipes">Recipes</a>
-        <a className="nav_link" href="#chefs">Chefs</a>
-        <a className="nav_link" href="#contact">Contacts</a>
-      </nav>
-    </header>
-        
-  );
+class Header extends Component {
+  render() {
+    const { toggleTheme } = this.props;
+    return (
+      <ThemeContext.Consumer>
+        {(theme) => (
+          <header className="header">
+            <Logo className="logo" />
+            <button type="button" onClick={toggleTheme}>Toggle Theme</button>
+            <nav className="nav">
+              <a className={`nav_link ${theme}`} href="#menu">Menu</a>
+              <a className={`nav_link ${theme}`} href="#recipes">Recipes</a>
+              <a className={`nav_link ${theme}`} href="#chefs">Chefs</a>
+              <a className={`nav_link ${theme}`} href="#contact">Contacts</a>
+            </nav>
+          </header>
+        )}
+      </ThemeContext.Consumer>         
+    );
+  }
 }
+
+Header.propTypes = {
+  toggleTheme: PropTypes.func.isRequired, 
+};
 
 export default Header;
