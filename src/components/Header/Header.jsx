4ec61a1c829307d1cf7846a6939099c69ec9db3e
Alena <olenanepokryta1997@gmail.com>
2023-04-10 00:04:25 +0300

    feat: [LSS-9] rewrite Header class component to hooks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from '../ThemeContext/ThemeContext';
 import Logo from '../Logo/Logo';
@@ -7,28 +7,27 @@ import toggleOn from '../../assets/icons/toggle-on.svg';
 
 import './sass/Header.sass';
 
-class Header extends Component {
-  render() {
-    const { toggleTheme } = this.props;
-    return (
-      <ThemeContext.Consumer>
-        {(theme) => (
-          <header className="header">
-            <Logo className="logo" />
-            <button type="button" onClick={toggleTheme} className={`header_button ${theme}`}>
-              {theme === 'dark' ? <img src={toggleOff} alt="toggleOff" /> : <img src={toggleOn} alt="toggleOn" /> } 
-            </button>
-            <nav className="nav">
-              <a className={`nav_link ${theme}`} href="#menu">Menu</a>
-              <a className={`nav_link ${theme}`} href="#recipes">Recipes</a>
-              <a className={`nav_link ${theme}`} href="#chefs">Chefs</a>
-              <a className={`nav_link ${theme}`} href="#contact">Contacts</a>
-            </nav>
-          </header>
-        )}
-      </ThemeContext.Consumer>         
-    );
-  }
+function Header(props) {
+  const { toggleTheme } = props;
+
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <header className="header">
+          <Logo className="logo" />
+          <button type="button" onClick={toggleTheme} className={`header_button ${theme}`}>
+            {theme === 'dark' ? <img src={toggleOff} alt="toggleOff" /> : <img src={toggleOn} alt="toggleOn" /> } 
+          </button>
+          <nav className="nav">
+            <a className={`nav_link ${theme}`} href="#menu">Menu</a>
+            <a className={`nav_link ${theme}`} href="#recipes">Recipes</a>
+            <a className={`nav_link ${theme}`} href="#chefs">Chefs</a>
+            <a className={`nav_link ${theme}`} href="#contact">Contacts</a>
+          </nav>
+        </header>
+      )}
+    </ThemeContext.Consumer>         
+  );
 }
 
 Header.propTypes = {
