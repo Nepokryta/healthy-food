3ec972b9ab00bcf146a2b37e3c10250009f5c4d5
Alena <olenanepokryta1997@gmail.com>
2023-04-09 23:59:16 +0300

    feat: [LSS-9] rewrite Contacts class component to hooks

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ContactsView from './ContactsView';
 
 import IcWatchLater from '../../assets/icons/ic_watch_later.svg';
@@ -7,67 +7,58 @@ import IcCall from '../../assets/icons/ic_call.svg';
 
 import './sass/Contact.sass';
 
-class Contacts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contact: [
-        {
-          id: 1,
-          src: IcWatchLater,
-          alt: 'ic_watch_later',
-          href: '/',
-          title: 'Today 10:00 am - 7:00 pm',
-          subtitle: 'Working hours',
-          target: '_self',
-          rel: '',
-        },
-        {
-          id: 2,
-          src: IcNearMe,
-          alt: 'ic_near_me',
-          href: 'https://goo.gl/maps/8VTd6biYdAWWSjAv5',
-          title: 'Velyka Vasylkivska 100',
-          subtitle: 'Get Directions',
-          target: '_blank',
-          rel: 'noopener noreferrer',
-        },
-        {
-          id: 3,
-          src: IcCall,
-          alt: 'ic_call',
-          href: 'tel:+80638332415',
-          title: '+38 (063)833 24 15',
-          subtitle: 'Call Online',
-          target: '_self',
-          rel: '',
-        },
+function Contacts() {
+  const [contact] = useState([
+    {
+      id: 1,
+      src: IcWatchLater,
+      alt: 'ic_watch_later',
+      href: '/',
+      title: 'Today 10:00 am - 7:00 pm',
+      subtitle: 'Working hours',
+      target: '_self',
+      rel: '',
+    },
+    {
+      id: 2,
+      src: IcNearMe,
+      alt: 'ic_near_me',
+      href: 'https://goo.gl/maps/8VTd6biYdAWWSjAv5',
+      title: 'Velyka Vasylkivska 100',
+      subtitle: 'Get Directions',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    },
+    {
+      id: 3,
+      src: IcCall,
+      alt: 'ic_call',
+      href: 'tel:+80638332415',
+      title: '+38 (063)833 24 15',
+      subtitle: 'Call Online',
+      target: '_self',
+      rel: '',
+    },
+  ]);
 
-      ],
-    };
-  }
+  const elements = contact.map((item) => (
+    <ContactsView
+      key={item.id}
+      src={item.src} 
+      alt={item.alt} 
+      href={item.href}
+      title={item.title}
+      subtitle={item.subtitle}
+      target={item.target}
+      rel={item.rel}
+    />
+  ));
 
-  render() {
-    const { contact } = this.state;
-
-    const elements = contact.map((item) => (
-      <ContactsView
-        key={item.id}
-        src={item.src} 
-        alt={item.alt} 
-        href={item.href}
-        title={item.title}
-        subtitle={item.subtitle}
-        target={item.target}
-        rel={item.rel}
-      />
-    ));
-    return (
-      <ul className="contact">
-        {elements}
-      </ul>
-    );
-  }
+  return (
+    <ul className="contact">
+      {elements}
+    </ul>
+  );
 }
 
 export default Contacts;
