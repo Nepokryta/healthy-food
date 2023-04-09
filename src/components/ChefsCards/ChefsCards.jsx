6fa9965482c41754b8c4dd4f6774626480fd124b
Alena <olenanepokryta1997@gmail.com>
2023-04-09 23:56:30 +0300

    feat: [LSS-9] rewrite ChefsCards class component to hooks

--- src/components/ChefsCards/ChefsCards.jsx
+++ src/components/ChefsCards/ChefsCards.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ChefCardView from './ChefCardView';
 
 import Avatar1 from '../../assets/img/avatar1.png';
@@ -16,78 +16,70 @@ import Img6 from '../../assets/img/img3-3.png';
 
 import './sass/ChefsCards.sass';
 
-class ChefsCards extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chefs: [
-        {
-          id: 1,
-          avatar: Avatar1,
-          avatarAlt: 'avatar1',
-          name: 'Gregory Flores',
-          title: 'Chef of the cold',
-          bigImg: BigImg1,
-          bigImgAlt: 'big_img1',
-          smallImg1: Img1,
-          smallImg1Alt: 'img1_2',
-          smallImg2: Img2,
-          smallImg2Alt: 'img1_3',
-        },
-        {
-          id: 2,
-          avatar: Avatar2,
-          avatarAlt: 'avatar2',
-          name: 'Annette Cooper',
-          title: 'Chef of the hot',
-          bigImg: BigImg2,
-          bigImgAlt: 'big_img2',
-          smallImg1: Img3,
-          smallImg1Alt: 'img2_2',
-          smallImg2: Img4,
-          smallImg2Alt: 'img2_3',
-        },
-        {
-          id: 3,
-          avatar: Avatar3,
-          avatarAlt: 'avatar3',
-          name: 'Greg Fox',
-          title: 'Сhef macro kitchen',
-          bigImg: BigImg3,
-          bigImgAlt: 'big_img3',
-          smallImg1: Img5,
-          smallImg1Alt: 'img3_2',
-          smallImg2: Img6,
-          smallImg2Alt: 'img3_3',
-        },
-      ]
-    };
-  }
+function ChefsCards() {
+  const [chefs] = useState([
+    {
+      id: 1,
+      avatar: Avatar1,
+      avatarAlt: 'avatar1',
+      name: 'Gregory Flores',
+      title: 'Chef of the cold',
+      bigImg: BigImg1,
+      bigImgAlt: 'big_img1',
+      smallImg1: Img1,
+      smallImg1Alt: 'img1_2',
+      smallImg2: Img2,
+      smallImg2Alt: 'img1_3',
+    },
+    {
+      id: 2,
+      avatar: Avatar2,
+      avatarAlt: 'avatar2',
+      name: 'Annette Cooper',
+      title: 'Chef of the hot',
+      bigImg: BigImg2,
+      bigImgAlt: 'big_img2',
+      smallImg1: Img3,
+      smallImg1Alt: 'img2_2',
+      smallImg2: Img4,
+      smallImg2Alt: 'img2_3',
+    },
+    {
+      id: 3,
+      avatar: Avatar3,
+      avatarAlt: 'avatar3',
+      name: 'Greg Fox',
+      title: 'Сhef macro kitchen',
+      bigImg: BigImg3,
+      bigImgAlt: 'big_img3',
+      smallImg1: Img5,
+      smallImg1Alt: 'img3_2',
+      smallImg2: Img6,
+      smallImg2Alt: 'img3_3',
+    },
+  ]);
 
-  render() {
-    const { chefs } = this.state;
-
-    const elements = chefs.map((item) => (
-      <ChefCardView 
-        key={item.id}
-        avatar={item.avatar}
-        avatarAlt={item.avatarAlt}
-        name={item.name}
-        title={item.title}
-        bigImg={item.bigImg}
-        bigImgAlt={item.bigImgAlt}
-        smallImg1={item.smallImg1}
-        smallImg1Alt={item.smallImg1Alt}
-        smallImg2={item.smallImg2}
-        smallImg2Alt={item.smallImg2Alt}
-      />
-    ));
-    return (
-      <div className="chefs__cards">
-        {elements}
-      </div>
-    );
-  }
+  const elements = chefs.map((item) => (
+    <ChefCardView 
+      key={item.id}
+      avatar={item.avatar}
+      avatarAlt={item.avatarAlt}
+      name={item.name}
+      title={item.title}
+      bigImg={item.bigImg}
+      bigImgAlt={item.bigImgAlt}
+      smallImg1={item.smallImg1}
+      smallImg1Alt={item.smallImg1Alt}
+      smallImg2={item.smallImg2}
+      smallImg2Alt={item.smallImg2Alt}
+    />
+  ));
+  
+  return (
+    <div className="chefs__cards">
+      {elements}
+    </div>
+  );
 }
 
 export default ChefsCards;
