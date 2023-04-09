977beac8cce9c58449dabcafb16060ac73c92f2e
Alena <olenanepokryta1997@gmail.com>
2023-04-10 00:09:41 +0300

    feat: [LSS-9] rewrite Slider class component to hooks

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from '../ThemeContext/ThemeContext';
 
@@ -7,37 +7,35 @@ import Arrow from '../../assets/icons/arrow.svg';
 
 import './sass/Slider.sass';
 
-class Slider extends Component {
-  render() {
-    const { slider } = this.props;
-    const elements = slider.map((item) => (
-      <SliderView key={item.key} src={item.src} alt={item.alt} />
-    ));
+function Slider(props) {
+  const { slider } = props;
+  const elements = slider.map((item) => (
+    <SliderView key={item.key} src={item.src} alt={item.alt} />
+  ));
       
-    return (
-      <ThemeContext.Consumer>
-        {(theme) => (
-          <div className="about__slider">
-            <h3 className={`about__subtitle ${theme}`}>
-              In aliqua ea ullamco ad est ex non deserunt nulla. 
-              Consectetur sint ea aliquip aliquip consectetur 
-              voluptate est. Eu minim dolore laboris enim mollit 
-              voluptate irure esse aliquip.
-            </h3>
-            <div className="cards">
-              {elements}
-            </div>
-            <button className={`arrow_btn ${theme} arrow_btn-left`} type="submit">
-              <img src={Arrow} alt="arrow" />
-            </button>
-            <button className={`arrow_btn ${theme} arrow_btn-right`} type="submit">
-              <img src={Arrow} alt="arrow" />
-            </button>
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <div className="about__slider">
+          <h3 className={`about__subtitle ${theme}`}>
+            In aliqua ea ullamco ad est ex non deserunt nulla. 
+            Consectetur sint ea aliquip aliquip consectetur 
+            voluptate est. Eu minim dolore laboris enim mollit 
+            voluptate irure esse aliquip.
+          </h3>
+          <div className="cards">
+            {elements}
           </div>
-        )}
-      </ThemeContext.Consumer> 
-    );
-  }
+          <button className={`arrow_btn ${theme} arrow_btn-left`} type="submit">
+            <img src={Arrow} alt="arrow" />
+          </button>
+          <button className={`arrow_btn ${theme} arrow_btn-right`} type="submit">
+            <img src={Arrow} alt="arrow" />
+          </button>
+        </div>
+      )}
+    </ThemeContext.Consumer> 
+  );
 }
 Slider.contextType = ThemeContext;
 
