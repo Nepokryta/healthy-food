5d5654ea0d3f3e6aab42a2c11fa5db68668be3e5
Alena <olenanepokryta1997@gmail.com>
2023-04-11 10:24:25 +0300

    feat: [LSS-9] rewrite DishCardView and OneDishCardView class component to hooks

--- src/components/DishCards/DishCardView.jsx
+++ src/components/DishCards/DishCardView.jsx
@@ -1,72 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from '../ThemeContext/ThemeContext';
 import OneDishCardView from './OneDishCardView';
 
 import './sass/DishCardView.sass';
 
-class DishCardView extends Component {
-  render() {
-    const {
-      dish, onSort, onRandomSort, onCardClick, onAddCardClick, onAddElement, onDeleteElement, onAddElementOnClick, 
-      activeCard, toggleActive, onDragStart, onDragOver, onDrop, onDragLeave, onDragEnd, 
-      onKeyDown, dragging
-    } = this.props;
+function DishCardView(props) {
+  const {
+    dish, onSort, onRandomSort, onCardClick, onAddCardClick, onAddElement, onDeleteElement, onAddElementOnClick, 
+    activeCard, toggleActive, onDragStart, onDragOver, onDrop, onKeyDown, dragging
+  } = props;
  
-    const elements = dish.map((item) => (
-      <ThemeContext.Consumer key={item.id}>
-        {(theme) => (
-          <div
-            className={item.id === activeCard || dragging 
-              ? `dish__card ${theme} active ShiftLeft-q-pressed` 
-              : `dish__card ${theme} inactive`}
-            onClick={() => toggleActive(item.id)}
-            draggable
-            onDragStart={(e) => onDragStart(e, item)}
-            onDragLeave={(e) => onDragLeave(e)}
-            onDragEnd={(e) => onDragEnd(e)}
-            onDragOver={(e) => onDragOver(e)}
-            onDrop={(e) => onDrop(e, item)}
-            onKeyDown={(e) => onKeyDown(e)}
-            tabIndex={0} 
-            role="button"
-          >
-            <OneDishCardView
-              showElement={item.showElement}
-              id={item.id} 
-              src={item.src}
-              alt={item.alt}
-              title={item.title}
-              subtitle={item.subtitle}
-              newSubtitle={item.newSubtitle}
-              description={item.description}
-              onCardClick={onCardClick} 
-              onAddCardClick={onAddCardClick}
-              onAddElement={onAddElement}
-              onDeleteElement={onDeleteElement}
-              onAddElementOnClick={onAddElementOnClick}
-            />
-          </div>
-        )}
-      </ThemeContext.Consumer>  
-    ));
+  const elements = dish.map((item) => (
+    <ThemeContext.Consumer key={item.id}>
+      {(theme) => (
+        <div
+          className={item.id === activeCard || dragging 
+            ? `dish__card ${theme} active ShiftLeft-q-pressed` 
+            : `dish__card ${theme} inactive`}
+          onClick={() => toggleActive(item.id)}
+          draggable
+          onDragStart={(e) => onDragStart(e, item)}
+          onDragOver={(e) => onDragOver(e)}
+          onDrop={(e) => onDrop(e, item)}
+          onKeyDown={(e) => onKeyDown(e)}
+          tabIndex={0} 
+          role="button"
+        >
+          <OneDishCardView
+            showElement={item.showElement}
+            id={item.id} 
+            src={item.src}
+            alt={item.alt}
+            title={item.title}
+            subtitle={item.subtitle}
+            newSubtitle={item.newSubtitle}
+            description={item.description}
+            onCardClick={onCardClick} 
+            onAddCardClick={onAddCardClick}
+            onAddElement={onAddElement}
+            onDeleteElement={onDeleteElement}
+            onAddElementOnClick={onAddElementOnClick}
+          />
+        </div>
+      )}
+    </ThemeContext.Consumer>  
+  ));
       
-    return (
-      <ThemeContext.Consumer>
-        {(theme) => (
-          <div className="container">
-            <div className="dish__cards">
-              {elements}
-            </div>
-            <div className="sortBtn">
-              <button className={`action-btn button ${theme}`} type="submit" onClick={onRandomSort}>RANDOM</button>
-              <button className={`action-btn button ${theme}`} type="submit" onClick={onSort}>SORT</button>
-            </div>
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <div className="container">
+          <div className="dish__cards">
+            {elements}
           </div>
-        )}
-      </ThemeContext.Consumer>  
-    );
-  }
+          <div className="sortBtn">
+            <button className={`action-btn button ${theme}`} type="submit" onClick={onRandomSort}>RANDOM</button>
+            <button className={`action-btn button ${theme}`} type="submit" onClick={onSort}>SORT</button>
+          </div>
+        </div>
+      )}
+    </ThemeContext.Consumer>  
+  );
 }
 
 DishCardView.propTypes = {
@@ -80,8 +75,6 @@ DishCardView.propTypes = {
   toggleActive: PropTypes.func.isRequired,
   activeCard: PropTypes.string.isRequired,
   onDragStart: PropTypes.func.isRequired,
-  onDragLeave: PropTypes.func.isRequired,
-  onDragEnd: PropTypes.func.isRequired,
   onDragOver: PropTypes.func.isRequired,
   onDrop: PropTypes.func.isRequired,
   dragging: PropTypes.bool.isRequired,
--- src/components/DishCards/OneDishCardView.jsx
+++ src/components/DishCards/OneDishCardView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from '../ThemeContext/ThemeContext';
 
@@ -6,68 +6,56 @@ import RatingStars from './RatingStars/RatingStars';
 import { ReactComponent as Close } from '../../assets/icons/close.svg';
 import screenImage from '../../assets/img/screenImage.png';
 
-class OneDishCardView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imageError: false
-    };
-  }
+function OneDishCardView(props) {
+  const [imageError, setImageError] = useState(false);
 
-  handleImageLoad = () => {
-    this.setState({
-      imageError: false,
-    });
+  const handleImageLoad = () => {
+    setImageError(false);
     console.log('Image uploaded!');
   };
 
-  handleImageError = () => {
-    this.setState({
-      imageError: true,
-    });
+  const handleImageError = () => {
+    setImageError(true);
   };
 
-  render() {
-    const {
-      id, src, alt, title, subtitle, newSubtitle, description, onCardClick, onAddCardClick, onAddElement,
-      onDeleteElement, onAddElementOnClick, showElement, 
-    } = this.props;
+  const {
+    id, src, alt, title, subtitle, newSubtitle, description, onCardClick, onAddCardClick, onAddElement,
+    onDeleteElement, onAddElementOnClick, showElement, 
+  } = props;
 
-    const { imageError } = this.state;
-    return (
-      <ThemeContext.Consumer>
-        {(theme) => (
-          <>
-            <button className="arrow-card" type="submit" onClick={() => onCardClick(id)}>
-              <Close />
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <>
+          <button className="arrow-card" type="submit" onClick={() => onCardClick(id)}>
+            <Close />
+          </button>
+          {imageError ? <img src={screenImage} alt="error" /> : <img src={src} alt={alt} onLoad={handleImageLoad} onError={handleImageError} />}
+          <div className={`dish__card-details ${theme}`}>
+            <h2 className={`dish__card-title ${theme}`}>{title}</h2>
+            <h3 className={`dish__card-subtitle ${theme}`}>{subtitle}</h3>
+            { showElement ? '' : <h3 className={`dish__card-newsubtitle ${theme}`}>{newSubtitle}</h3> }
+            <h4 className="dish__card-description">{description}</h4>
+            <button className={`activityBTN button ${theme}`} type="submit" onClick={() => onAddElement(id)}>MY FAVORITE</button>
+            <div className="botton_block">
+              <button className={`activityBTN button ${theme}`} type="submit" onClick={() => onDeleteElement(id)}>DELETE</button>
+              <button className={`activityBTN button ${theme}`} type="submit" onClick={() => onAddElementOnClick(id)}>ADD</button>
+            </div>
+          </div>
+          <div className="dish__card-action">
+            <RatingStars />
+            <button 
+              className={`activityBTN button ${theme}`} 
+              type="submit"
+              onClick={() => onAddCardClick(src, alt, title, subtitle, description, newSubtitle)}
+            >
+              ADD CARD
             </button>
-            {imageError ? <img src={screenImage} alt="error" /> : <img src={src} alt={alt} onLoad={this.handleImageLoad} onError={this.handleImageError} />}
-            <div className={`dish__card-details ${theme}`}>
-              <h2 className={`dish__card-title ${theme}`}>{title}</h2>
-              <h3 className={`dish__card-subtitle ${theme}`}>{subtitle}</h3>
-              { showElement ? '' : <h3 className={`dish__card-newsubtitle ${theme}`}>{newSubtitle}</h3> }
-              <h4 className="dish__card-description">{description}</h4>
-              <button className={`activityBTN button ${theme}`} type="submit" onClick={() => onAddElement(id)}>MY FAVORITE</button>
-              <div className="botton_block">
-                <button className={`activityBTN button ${theme}`} type="submit" onClick={() => onDeleteElement(id)}>DELETE</button>
-                <button className={`activityBTN button ${theme}`} type="submit" onClick={() => onAddElementOnClick(id)}>ADD</button>
-              </div>
-            </div>
-            <div className="dish__card-action">
-              <RatingStars />
-              <button 
-                className={`activityBTN button ${theme}`} 
-                type="submit"
-                onClick={() => onAddCardClick(src, alt, title, subtitle, description, newSubtitle)}
-              >
-                ADD CARD
-              </button>
-            </div>
-          </>
-        )}
-      </ThemeContext.Consumer>    
-    );
-  }
+          </div>
+        </>
+      )}
+    </ThemeContext.Consumer>    
+  );
 }
 
 OneDishCardView.propTypes = {
