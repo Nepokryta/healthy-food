097dac7bc681cd04f7ce73759d7ab0f1bafb9edd
Alena <olenanepokryta1997@gmail.com>
2023-04-21 01:59:56 +0300

    fix: [LSS-10] fix get data function to Contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -8,43 +8,53 @@ import IcCall from '../../assets/icons/ic_call.svg';
 
 import './sass/Contact.sass';
 
+const contactsData = [
+  {
+    id: 1,
+    src: IcWatchLater,
+    alt: 'ic_watch_later',
+    href: '/',
+    title: 'contacts.title1',
+    subtitle: 'contacts.subtitle1',
+    target: '_self',
+    rel: '',
+  },
+  {
+    id: 2,
+    src: IcNearMe,
+    alt: 'ic_near_me',
+    href: 'https://goo.gl/maps/8VTd6biYdAWWSjAv5',
+    title: 'contacts.title2',
+    subtitle: 'contacts.subtitle2',
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  },
+  {
+    id: 3,
+    src: IcCall,
+    alt: 'ic_call',
+    href: 'tel:+80638332415',
+    title: '+38 (063)833 24 15',
+    subtitle: 'contacts.subtitle3',
+    target: '_self',
+    rel: '',
+  },
+];
+
 function Contacts() {
   const { t, i18n } = useTranslation();
-  const [contact, setContact] = useState([]);
+  const [contact, setContact] = useState(contactsData.map((c) => ({ 
+    ...c, 
+    title: t(c.title), 
+    subtitle: t(c.subtitle) 
+  })));
 
   useEffect(() => {
-    setContact([
-      {
-        id: 1,
-        src: IcWatchLater,
-        alt: 'ic_watch_later',
-        href: '/',
-        title: t('contacts.title1'),
-        subtitle: t('contacts.subtitle1'),
-        target: '_self',
-        rel: '',
-      },
-      {
-        id: 2,
-        src: IcNearMe,
-        alt: 'ic_near_me',
-        href: 'https://goo.gl/maps/8VTd6biYdAWWSjAv5',
-        title: t('contacts.title2'),
-        subtitle: t('contacts.subtitle2'),
-        target: '_blank',
-        rel: 'noopener noreferrer',
-      },
-      {
-        id: 3,
-        src: IcCall,
-        alt: 'ic_call',
-        href: 'tel:+80638332415',
-        title: '+38 (063)833 24 15',
-        subtitle: t('contacts.subtitle3'),
-        target: '_self',
-        rel: '',
-      },
-    ]);
+    setContact(contactsData.map((c) => ({ 
+      ...c, 
+      title: t(c.title), 
+      subtitle: t(c.subtitle) 
+    })));
   }, [i18n.language, t]);
 
   return (
