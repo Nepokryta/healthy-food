c88b6a04e04fb877dfa9c5640f7b726c8f48cc34
Alena <olenanepokryta1997@gmail.com>
2023-04-20 18:13:47 +0300

    fix: [LSS-10] fix stateless and stateful components in RatingStars

--- src/components/DishCards/RatingStars/RatingStars.jsx
+++ src/components/DishCards/RatingStars/RatingStars.jsx
@@ -1,40 +1,11 @@
-import React, { Component } from 'react';
-
-import RatingStarView from './RatingStarView';
-import { StarValue } from '../../../constants/constants';
+import React from 'react';
+import RatingStarsView from './RatingStarsView';
 import '../sass/RatingStars.sass';
 
-class RatingStars extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: 4,
-    };
-  }
-
-  handleStarClick = (rating) => {
-    this.setState({ rating });
-  };
-    
-  render() {
-    const { rating } = this.state;
-    const stars = [];
-    for (let i = 1; i <= 5; i += 1) {
-      stars.push(
-        <RatingStarView 
-          key={i} 
-          filled={i <= rating}
-          onClick={() => this.handleStarClick(i)}
-        />
-      );
-    }
-    return (
-      <div className="dish__card-action-stars">
-        {stars}
-        <div className="dish__card-action-value">{ StarValue[rating] || 0 }</div>
-      </div>
-    );
-  }
+function RatingStars() {
+  return (
+    <RatingStarsView />
+  );
 }
 
 export default RatingStars;
