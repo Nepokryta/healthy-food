f28bebba1e5e4c0dfd67473eff5bff5fe0216714
Alena <olenanepokryta1997@gmail.com>
2023-04-20 16:21:22 +0300

    fix: [LSS-10] fix stateless and stateful components in RecipeCards

--- src/components/RecipeCards/RecipeCards.jsx
+++ src/components/RecipeCards/RecipeCards.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import RecipeCardView from './RecipeCardView';
 import withLoadingAndError from '../../hoc/withLoadingAndError';
-import { RECIPE_CARD_BIG, RECIPE_CARD_SMALL } from '../../constants/constants';
+import RecipeCardsView from './RecipeCardsView';
 
 import './sass/RecipeCards.sass';
 
@@ -20,41 +19,13 @@ function RecipeCards({ data }) {
     setRecipes(updatedRecipes);
   }, [data]);
 
-  const elements = recipes.map((item, index) => (
-    <RecipeCardView 
-      key={item.key} 
-      src={item.src}
-      alt={item.alt}
-      size={index === 0 ? RECIPE_CARD_BIG : RECIPE_CARD_SMALL}
-      subtitle={item.subtitle} 
-      title={item.title}
-      newSubtitle={item.newSubtitle}
-      linkToRecipe={item.linkToRecipe}
-      totalTime={item.totalTime}
-      totalWeight={item.totalWeight}
-    />
-  ));
-    
   return (
-    <div className="recipes__cards">
-      {elements}
-    </div> 
+    <RecipeCardsView recipes={recipes} />
   );
 }
 
 RecipeCards.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired, 
-      src: PropTypes.string.isRequired,
-      alt: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      subtitle: PropTypes.string.isRequired,
-      newSubtitle: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      showElement: PropTypes.bool
-    }).isRequired
-  ).isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired
 };
 
 export default withLoadingAndError(RecipeCards);
---
+++ src/components/RecipeCards/RecipeCardsView.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RecipeCardView from './RecipeCardView';
+import withLoadingAndError from '../../hoc/withLoadingAndError';
+import { RECIPE_CARD_BIG, RECIPE_CARD_SMALL } from '../../constants/constants';
+
+function RecipeCardsView({ recipes }) {
+  const elements = recipes.map((item, index) => (
+    <RecipeCardView 
+      key={item.key} 
+      src={item.src}
+      alt={item.alt}
+      size={index === 0 ? RECIPE_CARD_BIG : RECIPE_CARD_SMALL}
+      subtitle={item.subtitle} 
+      title={item.title}
+      newSubtitle={item.newSubtitle}
+      linkToRecipe={item.linkToRecipe}
+      totalTime={item.totalTime}
+      totalWeight={item.totalWeight}
+    />
+  ));
+    
+  return (
+    <div className="recipes__cards">
+      {elements}
+    </div> 
+  );
+}
+
+RecipeCardsView.propTypes = {
+  recipes: PropTypes.arrayOf(PropTypes.shape({})).isRequired
+};
+
+export default withLoadingAndError(RecipeCardsView);
