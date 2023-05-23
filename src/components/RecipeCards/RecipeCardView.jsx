5ebaa6ca6b82c6922e83691a533616b0e35ba349
Alena <olenanepokryta1997@gmail.com>
2023-05-23 16:29:43 +0300

    fix: [LSS-12] fix RecipeCardView to add links to small cards

--- src/components/RecipeCards/RecipeCardView.jsx
+++ src/components/RecipeCards/RecipeCardView.jsx
@@ -4,9 +4,9 @@ import { useTranslation } from 'react-i18next';
 import ThemeContext from '../ThemeContext/ThemeContext';
 import { RECIPE_CARD_BIG, RECIPE_CARD_SMALL } from '../../constants/constants';
 
-import { ReactComponent as Arrow } from '../../assets/icons/arrow.svg';
-import { ReactComponent as RemoveRedEye } from '../../assets/icons/ic_remove_red_eye.svg';
-import { ReactComponent as QuestionAnswer } from '../../assets/icons/ic_question_answer.svg';
+// import { ReactComponent as Arrow } from '../../assets/icons/arrow.svg';
+// import { ReactComponent as RemoveRedEye } from '../../assets/icons/ic_remove_red_eye.svg';
+// import { ReactComponent as QuestionAnswer } from '../../assets/icons/ic_question_answer.svg';
 
 import './sass/RecipeCardView.sass';
 
@@ -17,8 +17,8 @@ function RecipeCardView({
   title,
   newSubtitle,
   linkToRecipe,
-  totalTime,
-  totalWeight,
+  //   totalTime,
+  //   totalWeight,
   size
 }) {
   const theme = useContext(ThemeContext);
@@ -29,16 +29,16 @@ function RecipeCardView({
     <div className={className}>
       <img src={src} alt={alt} className="recipe-img" />
       <div className={`recipe-info ${theme}`}>
-        <button className="arrow-card" type="submit">
+        {/* <button className="arrow-card" type="submit">
           <Arrow className={`arrow ${theme}`} />
-        </button>
+        </button> */}
         <button className={`subtitle ${theme}`} type="submit">{newSubtitle}</button>
         <h3 className={`data ${theme}`}>{subtitle}</h3>
         <h2 className={`title ${theme}`}>{title}</h2>
         <h3 className={`name ${theme}`}>
-          <a href={linkToRecipe}>{t('recipe.btnView')}</a>
+          <a href={linkToRecipe} target="_blank" rel="noreferrer">{t('recipe.btnView')}</a>
         </h3>
-        <div className="action">
+        {/* <div className="action">
           <div className={`views ${theme}`}>
             <RemoveRedEye className="removeRedEye" />
             <div className="views-value">{totalWeight}</div>
@@ -47,7 +47,7 @@ function RecipeCardView({
             <QuestionAnswer className="questionAnswer" />
             <div className="comments-value">{totalTime}</div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
@@ -60,8 +60,8 @@ RecipeCardView.propTypes = {
   subtitle: PropTypes.string.isRequired,
   newSubtitle: PropTypes.string.isRequired,
   linkToRecipe: PropTypes.string.isRequired,
-  totalTime: PropTypes.number.isRequired,
-  totalWeight: PropTypes.string.isRequired,
+  //   totalTime: PropTypes.number.isRequired,
+  //   totalWeight: PropTypes.string.isRequired,
   size: PropTypes.oneOf([RECIPE_CARD_BIG, RECIPE_CARD_SMALL]).isRequired,
 };
 
